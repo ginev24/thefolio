61a662f46530694ce60d3122990551140935674c
ginev24 <@nevermind.201>
2026-04-17 12:22:36 +0800

    Fixed the code

--- backend/server.js
+++ backend/server.js
@@ -20,7 +20,7 @@ connectDB();
 // ── Global Middleware ────────────────────────────────────────────────
 const allowedOrigins = [
   'http://localhost:3000', 
-  'https://thefolio-ix39-git-main-ginev24s-projects.vercel.app'
+  'https://thefolio-ix39.vercel.app'
 ];
 
 app.use(cors({
